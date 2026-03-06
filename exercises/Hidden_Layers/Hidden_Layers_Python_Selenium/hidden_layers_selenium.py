5e76b5baa73a00a9e61de25380112f0e3cf14c70
EvenMoreH <huber.lewan@gmail.com>
2026-03-06 09:00:15 +0100

    Selenium hidden layers complete

--- exercises/Hidden_Layers/Hidden_Layers_Python_Selenium/hidden_layers_selenium.py
+++ exercises/Hidden_Layers/Hidden_Layers_Python_Selenium/hidden_layers_selenium.py
@@ -4,4 +4,19 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 class HiddenLayersPage:
-    pass
+    GREEN_BUTTON = (By.ID, "greenButton")
+    BLUE_BUTTON = (By.ID, "blueButton")
+
+    def __init__(self, driver: WebDriver):
+        self.driver = driver
+
+    def _wait(self, timeout: int = 5):
+        return WebDriverWait(self.driver, timeout)
+
+    def click_button_green(self):
+            button_g = self._wait(3).until(EC.element_to_be_clickable(self.GREEN_BUTTON))
+            button_g.click()
+
+    def click_button_blue(self):
+            button_b = self._wait(3).until(EC.element_to_be_clickable(self.BLUE_BUTTON))
+            button_b.click()
