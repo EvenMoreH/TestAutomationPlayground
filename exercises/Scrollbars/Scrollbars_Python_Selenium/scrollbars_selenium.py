5d51593e4d04ee4f8b38b552cdfd8ee32c3eb1fa
EvenMoreH <huber.lewan@gmail.com>
2026-03-17 09:05:32 +0100

    ActionChains used to scroll for elements, Scrollbars selenium complete

--- exercises/Scrollbars/Scrollbars_Python_Selenium/scrollbars_selenium.py
+++ exercises/Scrollbars/Scrollbars_Python_Selenium/scrollbars_selenium.py
@@ -2,6 +2,23 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.remote.webdriver import WebDriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.action_chains import ActionChains
 
 class ScrollbarsPage:
-    pass
+    PRIMARY_BUTTON = (By.ID, "hidingButton")
+
+    def __init__(self, driver: WebDriver):
+        self.driver = driver
+
+    def _wait(self, timeout: int = 5):
+        return WebDriverWait(self.driver, timeout)
+
+    def _scroll_to_button(self):
+        button = self._wait().until(EC.presence_of_element_located(self.PRIMARY_BUTTON))
+
+        ActionChains(self.driver).move_to_element(button).perform()
+        return button
+
+    def click_button(self):
+        hidden_button = self._scroll_to_button()
+        hidden_button.click()
--- exercises/Scrollbars/Scrollbars_Python_Selenium/test_scrollbars_selenium.py
+++ exercises/Scrollbars/Scrollbars_Python_Selenium/test_scrollbars_selenium.py
@@ -1,3 +1,9 @@
 from selenium.webdriver.remote.webdriver import WebDriver
 from scrollbars_selenium import ScrollbarsPage
 
+def test_click_hidden_btn(driver: WebDriver):
+    scrollbars_page = ScrollbarsPage(driver)
+
+    driver.get("http://uitestingplayground.com/scrollbars")
+
+    scrollbars_page.click_button()
