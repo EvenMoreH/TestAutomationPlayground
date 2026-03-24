bc9db51af9af00f84b8b438afdc409db970f955a
EvenMoreH <huber.lewan@gmail.com>
2026-03-24 09:13:31 +0100

    Added boilerplate for visibility tests for various button states

--- exercises/Visibility/Visibility_Python_Selenium/test_visibility_selenium.py
+++ exercises/Visibility/Visibility_Python_Selenium/test_visibility_selenium.py
@@ -1,7 +1,59 @@
 from selenium.webdriver.remote.webdriver import WebDriver
 from visibility_selenium import VisibilityPage
 
-def test_visibility_selenium(driver: WebDriver):
+def test_removed_btn_visibility_selenium(driver: WebDriver):
     visibility_page = VisibilityPage(driver)
 
-    driver.get("")
+    driver.get("http://uitestingplayground.com/visibility")
+
+    visibility_page.hide_btn_click()
+    result = visibility_page.is_invisible_removed_btn()
+    assert result == True
+
+def test_zero_width_btn_visibility_selenium(driver: WebDriver):
+    visibility_page = VisibilityPage(driver)
+
+    driver.get("http://uitestingplayground.com/visibility")
+
+    visibility_page.hide_btn_click()
+    pass
+
+def test_overlapped_btn_visibility_selenium(driver: WebDriver):
+    visibility_page = VisibilityPage(driver)
+
+    driver.get("http://uitestingplayground.com/visibility")
+
+    visibility_page.hide_btn_click()
+    pass
+
+def test_opacity_zero_btn_visibility_selenium(driver: WebDriver):
+    visibility_page = VisibilityPage(driver)
+
+    driver.get("http://uitestingplayground.com/visibility")
+
+    visibility_page.hide_btn_click()
+    pass
+
+def test_visibility_hidden_btn_visibility_selenium(driver: WebDriver):
+    visibility_page = VisibilityPage(driver)
+
+    driver.get("http://uitestingplayground.com/visibility")
+
+    visibility_page.hide_btn_click()
+    pass
+
+def test_none_btn_visibility_selenium(driver: WebDriver):
+    visibility_page = VisibilityPage(driver)
+
+    driver.get("http://uitestingplayground.com/visibility")
+
+    visibility_page.hide_btn_click()
+    pass
+
+def test_offscreen_btn_visibility_selenium(driver: WebDriver):
+    visibility_page = VisibilityPage(driver)
+
+    driver.get("http://uitestingplayground.com/visibility")
+
+    visibility_page.hide_btn_click()
+    pass
--- exercises/Visibility/Visibility_Python_Selenium/visibility_selenium.py
+++ exercises/Visibility/Visibility_Python_Selenium/visibility_selenium.py
@@ -4,9 +4,44 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 class VisibilityPage:
+    HIDE_BTN = (By.ID, "hideButton")
+    REMOVED_BTN = (By.ID, "removedButton")
+    ZERO_W_BUTTON = (By.ID, "zeroWidthButton")
+    OVERLAPPED_BTN = (By.ID, "overlappedButton")
+    OPACITY_ZERO_BTN = (By.ID, "transparentButton")
+    VISIBILITY_HIDDEN_BTN = (By.ID, "invisibleButton")
+    NONE_BTN = (By.ID, "notdisplayedButton")
+    OFFSCREEN_BTN = (By.ID, "offscreenButton")
 
     def __init__(self, driver: WebDriver):
         self.driver = driver
 
     def _wait(self, timeout: int = 5):
         return WebDriverWait(self.driver, timeout)
+
+    def hide_btn_click(self):
+        hide_btn = self._wait().until(EC.element_to_be_clickable(self.HIDE_BTN))
+        hide_btn.click()
+
+    def is_invisible_removed_btn(self):
+        removed_btn = self._wait().until(EC.invisibility_of_element_located(self.REMOVED_BTN))
+        return removed_btn
+
+    def is_invisible_zero_width_btn(self):
+        pass
+
+    def is_invisible_overlapped_btn(self):
+        pass
+
+    def is_invisible_opacity_zero_btn(self):
+        pass
+
+    def is_invisible_visibility_hidden_btn(self):
+        pass
+
+    def is_invisible_none_btn(self):
+        pass
+
+    def is_invisible_offscreen_btn(self):
+        pass
+
