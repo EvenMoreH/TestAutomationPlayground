367375eaff822c0979e32240ed48c6d93988a1d2
EvenMoreH <huber.lewan@gmail.com>
2026-04-10 14:25:31 +0200

    frames solved by python selenium; Selenium Python exercises complete

--- exercises/Frames/Frames_Python_Selenium/frames_selenium.py
+++ exercises/Frames/Frames_Python_Selenium/frames_selenium.py
@@ -3,10 +3,61 @@ from selenium.webdriver.remote.webdriver import WebDriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
+Locator = tuple[str, str]
+
 class FramesPage:
+    OUTER_FRAME = (By.ID, "frame-outer")
+    INNER_FRAME = (By.ID, "frame-inner")
+    EDIT_BUTTON = (By.CSS_SELECTOR, "[data-action='edit']")
+    SUBMIT_BUTTON = (By.XPATH, ".//button[text()='Submit']")
+    CLICK_ME_BUTTON = (By.NAME, "my-button")
+    PRIMARY_BUTTON = (By.XPATH, ".//button[@class='btn-class']")
+    RESULT_BANNER = (By.ID, "result")
 
     def __init__(self, driver: WebDriver):
         self.driver = driver
 
     def _wait(self, timeout: int = 5):
+        """Return a WebDriverWait instance with the given timeout."""
         return WebDriverWait(self.driver, timeout)
+
+    def _switch_frame(self, locator: Locator) -> None:
+        """Wait for a frame to be present and switch to it."""
+        iframe = self._wait().until(EC.presence_of_element_located(locator))
+        self.driver.switch_to.frame(iframe)
+
+    def _click(self, button: Locator) -> None:
+        """Wait for a button to be clickable and click it."""
+        self._wait().until(EC.element_to_be_clickable(button)).click()
+
+    def switch_to_outer_frame(self) -> None:
+        """Switch the driver context to the outer frame."""
+        self._switch_frame(self.OUTER_FRAME)
+
+    def switch_to_inner_frame(self) -> None:
+        """Switch the driver context to the inner frame."""
+        self._switch_frame(self.INNER_FRAME)
+
+    def wait_for_result_update(self, expected_result: str) -> None:
+        """Wait until the result banner contains the expected text."""
+        self._wait(2).until(EC.text_to_be_present_in_element(self.RESULT_BANNER, expected_result))
+
+    def read_result(self) -> str:
+        """Return the current text of the result banner."""
+        return self._wait().until(EC.presence_of_element_located(self.RESULT_BANNER)).text
+
+    def click_edit_button(self) -> None:
+        """Click the Edit button."""
+        self._click(self.EDIT_BUTTON)
+
+    def click_submit_button(self) -> None:
+        """Click the Submit button."""
+        self._click(self.SUBMIT_BUTTON)
+
+    def click_click_me_button(self) -> None:
+        """Click the Click Me button."""
+        self._click(self.CLICK_ME_BUTTON)
+
+    def click_primary_button(self) -> None:
+        """Click the primary button."""
+        self._click(self.PRIMARY_BUTTON)
--- exercises/Frames/Frames_Python_Selenium/test_frames_selenium.py
+++ exercises/Frames/Frames_Python_Selenium/test_frames_selenium.py
@@ -2,7 +2,54 @@ from selenium.webdriver.remote.webdriver import WebDriver
 from exercises.base_page_selenium import BasePage
 from frames_selenium import FramesPage
 
-def test_frames_selenium(driver: WebDriver):
-    frames_page = FramesPage(driver)
+URL = "http://uitestingplayground.com/frames"
+RESULT = "Button pressed: "
 
-    driver.get("")
+def test_frames_outer_buttons_selenium(driver: WebDriver):
+    frames_page = FramesPage(driver)
+    page = BasePage(driver)
+
+    driver.get(URL)
+    assert page.is_loaded()
+
+    frames_page.switch_to_outer_frame()
+    frames_page.click_edit_button()
+    frames_page.wait_for_result_update(RESULT + "Edit")
+    assert frames_page.read_result() == RESULT + "Edit"
+
+    frames_page.click_submit_button()
+    frames_page.wait_for_result_update(RESULT + "Submit")
+    assert frames_page.read_result() == RESULT + "Submit"
+
+    frames_page.click_click_me_button()
+    frames_page.wait_for_result_update(RESULT + "Click me")
+    assert frames_page.read_result() == RESULT + "Click me"
+
+    frames_page.click_primary_button()
+    frames_page.wait_for_result_update(RESULT + "Primary")
+    assert frames_page.read_result() == RESULT + "Primary"
+
+def test_frames_inner_buttons_selenium(driver: WebDriver):
+    frames_page = FramesPage(driver)
+    page = BasePage(driver)
+
+    driver.get(URL)
+    assert page.is_loaded()
+
+    frames_page.switch_to_outer_frame()
+    frames_page.switch_to_inner_frame()
+    frames_page.click_edit_button()
+    frames_page.wait_for_result_update(RESULT + "Edit")
+    assert frames_page.read_result() == RESULT + "Edit"
+
+    frames_page.click_submit_button()
+    frames_page.wait_for_result_update(RESULT + "Submit")
+    assert frames_page.read_result() == RESULT + "Submit"
+
+    frames_page.click_click_me_button()
+    frames_page.wait_for_result_update(RESULT + "Click me")
+    assert frames_page.read_result() == RESULT + "Click me"
+
+    frames_page.click_primary_button()
+    frames_page.wait_for_result_update(RESULT + "Primary")
+    assert frames_page.read_result() == RESULT + "Primary"
